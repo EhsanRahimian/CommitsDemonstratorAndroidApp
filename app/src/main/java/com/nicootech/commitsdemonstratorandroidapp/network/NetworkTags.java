ea23ea08b61c977ef91bda55f3c770e51984e385
Ethan Rahimian <nicootech@gmail.com>
2021-01-17 11:08:43 -0800

    COMMITS_URL Entered.

---
+++ app/src/main/java/com/nicootech/commitsdemonstratorandroidapp/network/NetworkTags.java
@@ -0,0 +1,6 @@
+package com.nicootech.commitsdemonstratorandroidapp.network;
+
+public interface NetworkTags {
+    String COMMITS_URL = "https://api.github.com/repos/EhsanRahimian/CommitsDemonstratorAndroidApp/commits";
+
+}
