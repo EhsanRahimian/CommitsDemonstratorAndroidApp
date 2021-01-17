77cdaeb701eec7b44c955e0b6b16916086cd44a9
Ethan Rahimian <nicootech@gmail.com>
2021-01-17 09:34:01 -0800

    Working on MainActivity.java, it implements AsyncTaskInterface

--- app/src/main/java/com/nicootech/commitsdemonstratorandroidapp/MainActivity.java
+++ app/src/main/java/com/nicootech/commitsdemonstratorandroidapp/MainActivity.java
@@ -1,14 +1,31 @@
 package com.nicootech.commitsdemonstratorandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.nicootech.commitsdemonstratorandroidapp.model.Commit;
+import com.nicootech.commitsdemonstratorandroidapp.network.AsyncTaskInterface;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AsyncTaskInterface {
+    private static final String TAG = "MainActivity";
+    private ProgressDialog mProgressDialog;
+    private List<Commit> mCommitList;
+    private RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //commit change test
+    }
+
+    @Override
+    public void onTaskCompleted(String response, String urlIdentifier) {
+
     }
 }
