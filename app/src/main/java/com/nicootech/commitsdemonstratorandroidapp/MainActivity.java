d424441d2db01abc7c28a65301b6be6ae047bdc4
Ethan Rahimian <nicootech@gmail.com>
2021-01-17 19:23:11 -0800

    Checking Internet Connection, Showing the progress Dialog

--- app/src/main/java/com/nicootech/commitsdemonstratorandroidapp/MainActivity.java
+++ app/src/main/java/com/nicootech/commitsdemonstratorandroidapp/MainActivity.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.nicootech.commitsdemonstratorandroidapp.adapter.CommitAdapter;
 import com.nicootech.commitsdemonstratorandroidapp.model.Commit;
 import com.nicootech.commitsdemonstratorandroidapp.network.AsyncTaskInterface;
 import com.nicootech.commitsdemonstratorandroidapp.network.NetworkCall;
 import com.nicootech.commitsdemonstratorandroidapp.network.NetworkTags;
+import com.nicootech.commitsdemonstratorandroidapp.utils.Utility;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,6 +30,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AsyncTaskInterface {
+
     private static final String TAG = "MainActivity";
     private ProgressDialog mProgressDialog;
     private List<Commit> mCommitList;
@@ -32,8 +41,27 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskInterfac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (internetConnectionCheck(MainActivity.this)) {
 
-        initializeViews();
+
+              final Toast toast =Toast.makeText(getApplicationContext(), "Internet Connection is available !!!", Toast.LENGTH_LONG);
+              toast.show();
+              Handler handler = new Handler();
+              handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        toast.cancel();
+                    }
+
+              }, 1200);
+              initializeViews();
+
+        } else
+
+        {
+            Toast.makeText(getApplicationContext(), "No Internet Connection Try Later !", Toast.LENGTH_LONG).show();
+        }
+
     }
     private void initializeViews() {
         findViewById(R.id.btn_fetch).setOnClickListener(new View.OnClickListener() {
@@ -52,6 +80,7 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskInterfac
     }
     private void getCommitsData() {
 
+        mProgressDialog = Utility.showProgressDialog(this, getString(R.string.fetching_commits));
         new NetworkCall(null, this, NetworkTags.COMMITS_URL).execute();
     }
     @Override
@@ -66,7 +95,7 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskInterfac
         try {
             JSONArray jsonArray = new JSONArray(response);
             // Logic to fetch 25 commits only latest.
-            for (int i=0;i<15;i++) {
+            for (int i=0;i<25;i++) {
                 Commit newCommit = new Commit();
                 newCommit.setmSha("Commit Hash: "+jsonArray.getJSONObject(i).getString("sha"));
                 newCommit.setmAuthor("Author: "+jsonArray.getJSONObject(i).getJSONObject("commit").getJSONObject("author").getString("name"));
@@ -78,5 +107,27 @@ public class MainActivity extends AppCompatActivity implements AsyncTaskInterfac
             e.printStackTrace();
         }
 
+    }
+    public static boolean internetConnectionCheck(Activity CurrentActivity) {
+        Boolean Connected = false;
+        ConnectivityManager connectivity = (ConnectivityManager) CurrentActivity.getApplicationContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null) for (int i = 0; i < info.length; i++)
+                if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                    Log.e("My Network is: ", "Connected");
+                    Connected = true;
+                } else {}
+        } else {
+            Log.e("My Network is: ", "Not Connected");
+
+            Toast.makeText(CurrentActivity.getApplicationContext(),
+                    "Please Check Your internet connection",
+                    Toast.LENGTH_LONG).show();
+            Connected = false;
+
+        }
+        return Connected;
     }
 }
